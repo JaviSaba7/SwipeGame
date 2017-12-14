c88034f9607f0ee96db2ea15c53d81f86f089a41
JaviSaba7 <javiersabariego@hotmail.com>
2017-12-14 15:45:36 +0100

    SwipeGame (in progress)
    Changes:
    - Enemy shoots to player (not finished)

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -2,30 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBehaviour : MonoBehaviour {
+public class EnemyBehaviour : MonoBehaviour
+{
+    public Transform target;
+    public int moveSpeed;
+    public int rotationSpeed;
+    public int maxRange;
+    public int minRange;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    //public float speed;
+    AudioSource audio;
+    bool dispaross = false;
+    private float nextFire = 0.0F;
+    public float fireRate = 0.5F;
 
-    public GameObject enemy;
-    public float positionX;
-    
-	// Use this for initialization
-	void Start ()
+    private Transform myTransform;
+
+    void Awake()
     {
-        
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        myTransform = transform;
+    }
+
+    void Start()
     {
-        
-        if (enemy.transform.position.x <= -2)
+        GameObject go = GameObject.FindGameObjectWithTag("Player");
+        target = go.transform;
+    }
+
+
+    void Update()
+    {
+        Debug.DrawLine(target.position, myTransform.position, Color.yellow);
+
+        //watching player when arrives at the correct position
+        myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+
+        if ((Vector3.Distance(transform.position, target.position) < maxRange) && (Vector3.Distance(transform.position, target.position) > minRange) && Time.time > nextFire)
         {
-            
+            transform.LookAt(target);
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            /*if (Input.GetKey(KeyCode "B"))
+            {
+                Fire();
+            }*/
         }
-        else
-        {
-            positionX--;
-        }
-       
-        
-	}
+    }
+
+    void Fire()
+    {
+        nextFire = Time.time + fireRate;
+        var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        Destroy(bullet, 2f);
+    }
+
 }
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    Transform player;               // Reference to the player's position.
+    public bool activeEnemies = true;
+
+    public NavMeshAgent nav;               // Reference to the nav mesh agent.
+
+
+    void Awake()
+    {
+        // Set up the references.
+        
+       
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        // If the enemy and the player have health left...
+        if (activeEnemies)
+        {
+            // ... set the destination of the nav mesh agent to the player.
+            nav.SetDestination(player.position);
+        }
+        // Otherwise...
+        else
+        {
+            // ... disable the nav mesh agent.
+            nav.enabled = false;
+        }
+    }
+}
