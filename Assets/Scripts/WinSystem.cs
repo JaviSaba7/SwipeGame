19cd30f58a6fc8a1afe58c06ea294c8cafa79dcb
JaviSaba7 <javiersabariego@hotmail.com>
2018-01-19 15:29:18 +0100

    WallyGame
    Changes:
    - Creating a new videogame for Defyu

--- Assets/Scripts/WinSystem.cs
+++ Assets/Scripts/WinSystem.cs
@@ -5,11 +5,18 @@ using System;
 using UnityEngine.UI;
 public class WinSystem : MonoBehaviour {
 
-    public SwipeBall1 swipeSystem;
     public float timeToStart = 3.0f;
-    public Text counterToStart;
-	// Use this for initialization
-	void Start ()
+    public float timeofGame = 30.0f;
+    public float score;
+    public Text scoreText;
+    public Text counterToStartText;
+    public Text gameTimeText;
+    public bool startGame;
+    public bool startToPlay;
+
+    public float timeOfGame = 30.0f;
+    // Use this for initialization
+    void Start ()
     {
 		
 	}
@@ -17,37 +24,41 @@ public class WinSystem : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if(swipeSystem.startGame)
+        if (Input.GetKeyDown(KeyCode.S))
         {
-            counterToStart.text = timeToStart.ToString("0") + "s";
+
+            startGame = true;
+
+        }
+
+        if (startGame)
+        {
+            counterToStartText.text = timeToStart.ToString("0") + "s";
+            timeToStart -= Time.deltaTime;
         }
 
         if(timeToStart < 0)
         {
-            counterToStart.enabled = false;
-            swipeSystem.activeTime = true;
+            counterToStartText.enabled = false;
+            gameTimeText.enabled = true;
+            startToPlay = true;
         }
 
-        if (swipeSystem.startGame) timeToStart -= Time.deltaTime;
-
-        if (swipeSystem.activeTime) swipeSystem.timeGame -= Time.deltaTime;
-        swipeSystem.counterText.text = swipeSystem.timeGame.ToString("00.00") + "s";
-
-        if (swipeSystem.timeGame <= 0)
+        if(startToPlay)
         {
-            swipeSystem.winText.SetActive(true);
-            swipeSystem.timeGame = 0;
-            swipeSystem.activeTime = false;
+            gameTimeText.text = timeofGame.ToString("0");
+            timeofGame -= Time.deltaTime;
         }
-        if (swipeSystem.timeGame == 0) Debug.Log("YOU WIN!");
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            swipeSystem.timeGame = 0;
-            if (swipeSystem.timeGame == 0)
-            {
-                Debug.Log("YOU WIN!");
-            }
-            swipeSystem.activeTime = false;
-        }
+
+        scoreText.text = score.ToString("");
+        
+
+        score = 3;
+
+
+
+
+        if (timeOfGame == 0) Debug.Log("YOU WIN!");
+      
     }
 }
